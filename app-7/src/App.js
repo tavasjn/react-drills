8f822cc8c656f04edca1a62cf8d432e814b13b8f
Tavas Neuwirth <tavasjn_24@hotmail.com>
2019-10-22 16:35:43 -0600

    Hooks Done

--- app-7/src/App.js
+++ app-7/src/App.js
@@ -2,31 +2,32 @@ import React, { Component } from "react";
 import "./App.css";
 import NewTask from './NewTask';
 import List from './List';
+import Hooks from './Components/Hooks';
+
 
 class App extends Component {
 
-  constructor() {
-    super();
+  // constructor() {
+  //   super();
 
-    this.state = {
-      list: []
-    };
+  //   this.state = {
+  //     list: []
+  //   };
 
-    this.handleAddTask = this.handleAddTask.bind(this);
-  }
+  //   this.handleAddTask = this.handleAddTask.bind(this);
+  // }
 
 
-  handleAddTask(task) {
-    this.setState({ list: [...this.state.list, task] });
-  }
+  // handleAddTask(task) {
+  //   this.setState({ list: [...this.state.list, task] });
+  // }
 
 
   render() {
     return (
       <div className="App">
         <h1>My to-do list:</h1>
-        <NewTask add={this.handleAddTask} />
-        <List tasks={this.state.list} />
+        <Hooks />
       </div>
     );
   }
---
+++ app-7/src/Components/Hooks.js
@@ -0,0 +1,28 @@
+import React, {useState} from 'react';
+
+const Hooks = () => {
+
+    const [list, setList] = useState([]);
+    const [newItem, setNewItem] = useState('');
+
+    const addItem = () => {
+        list.push(newItem);
+        setNewItem('')
+    }
+
+    return(
+        <div>
+            <div>
+                MY TO-DO LIST:
+            </div>
+            <div>
+                <input onChange={e => setNewItem(e.target.value)}/>
+                <button onClick={addItem}>Add</button>
+                <br />
+                {list.map(e => (<div>{e}</div>))}
+            </div>
+        </div>
+    )
+}
+
+export default Hooks;
