a3a232f6b2c7275600463ab7644c9b71a76318d4
Tavas Neuwirth <tavasjn_24@hotmail.com>
2019-10-22 15:59:04 -0600

    Hooks Done

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
 import "./App.css";
+import Hooks from './Components/Hooks';
 
-import Image from './Image';
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Image myImage={'https://scontent-lax3-2.xx.fbcdn.net/v/t1.0-9/49368501_10215469784532272_5920973186198929408_o.jpg?_nc_cat=103&_nc_oc=AQnQ2W9ThcIV2yCMigQU2_MKZIyBfCUzx7NxasRrjGmfJYK-Inpug1Wh2Zd4Z1J15fU&_nc_ht=scontent-lax3-2.xx&oh=d400bfc1b73df8ed783da83aa042f023&oe=5E3BBD20'}/>
+        <h2>Hooks</h2>
+        <Hooks />
       </div>
     );
   }
---
+++ app-5/src/Components/Hooks.js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react';
+
+const Hooks = () => {
+
+
+
+    return(
+        <div>
+            <img src={'https://scontent-lax3-2.xx.fbcdn.net/v/t1.0-9/49368501_10215469784532272_5920973186198929408_o.jpg?_nc_cat=103&_nc_oc=AQnQ2W9ThcIV2yCMigQU2_MKZIyBfCUzx7NxasRrjGmfJYK-Inpug1Wh2Zd4Z1J15fU&_nc_ht=scontent-lax3-2.xx&oh=d400bfc1b73df8ed783da83aa042f023&oe=5E3BBD20'} />
+        </div>
+    )
+}
+
+export default Hooks;
+
+
+
